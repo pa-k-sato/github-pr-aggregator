e963b1be4d300ecc206af35fefdffe1bae617d9f
kenji sato <63083111+pa-k-sato@users.noreply.github.com>
2022-11-18 09:19:07 +0900

    コマンドで集計処理を呼ぶ

--- src/main/kotlin/k/sato/pr/aggregator/commands/PrCommands.kt
+++ src/main/kotlin/k/sato/pr/aggregator/commands/PrCommands.kt
@@ -1,5 +1,7 @@
 package k.sato.pr.aggregator.commands
 
+import k.sato.pr.aggregator.domain.Work
+import k.sato.pr.aggregator.domain.WorkAggregator
 import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
 
@@ -7,6 +9,14 @@ import org.springframework.shell.standard.ShellMethod
 class PrCommands {
     @ShellMethod("aggregate pull requests")
     fun aggregate() {
-        println("hoge")
+        val aggregator = WorkAggregator()
+        aggregator.add(
+            Work("2022-11-07T09:00:00Z", "2022-11-07T17:00:00Z", 150, 20, 3)
+        )
+        aggregator.add(
+            Work("2022-11-08T09:00:00Z", "2022-11-09T11:00:00Z", 50, 50, 10)
+        )
+
+        println("平均期間： ${aggregator.durationAverage()}")
     }
 }
